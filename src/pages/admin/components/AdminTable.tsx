fcef2fdb936027460022855c25a2843e2bed031a
hkgnp <47936793+hkgnp@users.noreply.github.com>
2023-10-28 01:34:14 +0800

    chore: cleanup sorting

---
+++ src/pages/admin/components/AdminTable.tsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import { Table, Thead, Tbody, Tr, Th, Td, chakra } from "@chakra-ui/react";
+import { TriangleDownIcon, TriangleUpIcon } from "@chakra-ui/icons";
+import {
+  useReactTable,
+  flexRender,
+  getCoreRowModel,
+  SortingState,
+  getSortedRowModel,
+} from "@tanstack/react-table";
+import { TableForm, columns } from "../table-helpers/columns";
+
+type AdminTableProps = {
+  data: TableForm[];
+};
+
+const AdminTable = ({ data }: AdminTableProps) => {
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const table = useReactTable({
+    data,
+    columns,
+    state: {
+      sorting,
+    },
+    getCoreRowModel: getCoreRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    enableSortingRemoval: false,
+  });
+
+  return (
+    <Table>
+      <Thead>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <Tr key={headerGroup.id}>
+            {headerGroup.headers.map((header) => {
+              // see https://tanstack.com/table/v8/docs/api/core/column-def#meta to type this correctly
+              const meta: any = header.column.columnDef.meta;
+              return (
+                <Th
+                  key={header.id}
+                  onClick={header.column.getToggleSortingHandler()}
+                  isNumeric={meta?.isNumeric}
+                >
+                  {flexRender(
+                    header.column.columnDef.header,
+                    header.getContext(),
+                  )}
+
+                  <chakra.span pl="4">
+                    {header.column.getIsSorted() ? (
+                      header.column.getIsSorted() === "desc" ? (
+                        <TriangleDownIcon aria-label="sorted descending" />
+                      ) : (
+                        <TriangleUpIcon aria-label="sorted ascending" />
+                      )
+                    ) : null}
+                  </chakra.span>
+                </Th>
+              );
+            })}
+          </Tr>
+        ))}
+      </Thead>
+      <Tbody>
+        {table.getRowModel().rows.map((row) => (
+          <Tr key={row.id}>
+            {row.getVisibleCells().map((cell) => {
+              // see https://tanstack.com/table/v8/docs/api/core/column-def#meta to type this correctly
+              const meta: any = cell.column.columnDef.meta;
+              return (
+                <Td key={cell.id} isNumeric={meta?.isNumeric}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </Td>
+              );
+            })}
+          </Tr>
+        ))}
+      </Tbody>
+    </Table>
+  );
+};
+
+export default AdminTable;
--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,112 +1,19 @@
-import React, { useEffect, useState } from "react";
-import {
-  Text,
-  Table,
-  Thead,
-  Tbody,
-  Tr,
-  Th,
-  Td,
-  chakra,
-} from "@chakra-ui/react";
-import { TriangleDownIcon, TriangleUpIcon } from "@chakra-ui/icons";
-import {
-  useReactTable,
-  flexRender,
-  getCoreRowModel,
-  SortingState,
-  getSortedRowModel,
-} from "@tanstack/react-table";
+import { Text } from "@chakra-ui/react";
 import { trpc } from "@/utils/trpc-hooks";
-import { TableForm, columns } from "./table-helpers/columns";
+import AdminTable from "./components/AdminTable";
 
 const AdminDashboard = () => {
-  const [sorting, setSorting] = useState<SortingState>([]);
-  const [data, setData] = useState<TableForm[]>([]);
   const res = trpc.getAllForms.useQuery();
-  const formData = res.data?.map((d) => ({
+  const data = res.data?.map((d) => ({
     ...d,
     msw_name: d.msw_name.name,
     survey_date: new Date(d.survey_date),
   }));
-  console.log(formData);
-
-  useEffect(() => {
-    if (formData) {
-      setData(formData);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  const table = useReactTable({
-    data,
-    columns,
-    state: {
-      sorting,
-    },
-    getCoreRowModel: getCoreRowModel(),
-    onSortingChange: setSorting,
-    getSortedRowModel: getSortedRowModel(),
-    enableSortingRemoval: false,
-  });
 
   return (
     <>
       <Text textStyle="h4">AdminDashboard</Text>
-      <Table>
-        <Thead>
-          {data &&
-            table.getHeaderGroups().map((headerGroup) => (
-              <Tr key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  // see https://tanstack.com/table/v8/docs/api/core/column-def#meta to type this correctly
-                  const meta: any = header.column.columnDef.meta;
-                  return (
-                    <Th
-                      key={header.id}
-                      onClick={header.column.getToggleSortingHandler()}
-                      isNumeric={meta?.isNumeric}
-                    >
-                      {flexRender(
-                        header.column.columnDef.header,
-                        header.getContext(),
-                      )}
-
-                      <chakra.span pl="4">
-                        {header.column.getIsSorted() ? (
-                          header.column.getIsSorted() === "desc" ? (
-                            <TriangleDownIcon aria-label="sorted descending" />
-                          ) : (
-                            <TriangleUpIcon aria-label="sorted ascending" />
-                          )
-                        ) : null}
-                      </chakra.span>
-                    </Th>
-                  );
-                })}
-              </Tr>
-            ))}
-        </Thead>
-        <Tbody>
-          {data &&
-            table.getRowModel().rows.map((row) => (
-              <Tr key={row.id}>
-                {row.getVisibleCells().map((cell) => {
-                  // see https://tanstack.com/table/v8/docs/api/core/column-def#meta to type this correctly
-                  const meta: any = cell.column.columnDef.meta;
-                  return (
-                    <Td key={cell.id} isNumeric={meta?.isNumeric}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext(),
-                      )}
-                    </Td>
-                  );
-                })}
-              </Tr>
-            ))}
-        </Tbody>
-      </Table>{" "}
+      {data && <AdminTable data={data} />}
     </>
   );
 };
