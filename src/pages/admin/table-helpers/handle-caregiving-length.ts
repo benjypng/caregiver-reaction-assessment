8d1650220f2a50a47d44773f86534ec46b4403b3
hkgnp <47936793+hkgnp@users.noreply.github.com>
2023-10-27 21:58:31 +0800

    map values

---
+++ src/pages/admin/table-helpers/handle-caregiving-length.ts
@@ -0,0 +1,19 @@
+import { caregiving_length } from "@prisma/client";
+
+export const handleCaregivingLength = (str: caregiving_length) => {
+  switch (str) {
+    case "BELOW_1_YEAR":
+      return "Less than 1 year";
+    case "FROM_1_TO_3_YEARS":
+      return "1 to 3 years";
+    case "FROM_4_TO_6_YEARS":
+      return "4 to 6 years";
+    case "FROM_7_TO_10_YEARS":
+      return "7 to 10 years";
+    case "FROM_11_TO_20_YEARS":
+      return "11 to 20 years";
+    case "ABOVE_21_YEARS":
+      return "More than 21 years";
+    default:
+  }
+};
---
+++ src/pages/admin/table-helpers/handle-questions.ts
@@ -0,0 +1,15 @@
+export const handleQuestions = (str: string) => {
+  switch (str) {
+    case "1":
+      return "Strongly Agree";
+    case "2":
+      return "Agree";
+    case "3":
+      return "Neither Agree nor Disagree";
+    case "4":
+      return "Disagree";
+    case "5":
+      return "Strongly Disagree";
+    default:
+  }
+};
