b021264db5a8b6f087ae9f6c19a20dd260448b94
hkgnp <ben@pngs.cc>
2024-04-06 09:32:19 +0800

    feat: conditional display of signin signout

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { Flex, Text } from "@chakra-ui/react";
 import NextLink from "next/link";
+import { useSession } from "next-auth/react";
 
 const NavMenu = () => {
+  const { data: session, status } = useSession();
+  console.log(status);
+
   return (
     <Flex
       pos="fixed"
@@ -14,24 +18,27 @@ const NavMenu = () => {
       mt="0"
       p="3"
     >
-      Links:{" "}
       <NextLink passHref href="/api/auth/signin">
         <Text textDecoration="underline" color="white">
           Sign In
         </Text>
       </NextLink>
-      {" | "}
-      <NextLink passHref href="/api/auth/signout">
-        <Text textDecoration="underline" color="white">
-          Sign Out
-        </Text>
-      </NextLink>
-      {" | "}
-      <NextLink passHref href="/admin">
-        <Text textDecoration="underline" color="white">
-          Admin (must be signed in)
-        </Text>
-      </NextLink>
+      {status === "authenticated" && (
+        <>
+          {" | "}
+          <NextLink passHref href="/api/auth/signout">
+            <Text textDecoration="underline" color="white">
+              Sign Out
+            </Text>
+          </NextLink>
+          {" | "}
+          <NextLink passHref href="/admin">
+            <Text textDecoration="underline" color="white">
+              Admin (must be signed in)
+            </Text>
+          </NextLink>
+        </>
+      )}
     </Flex>
   );
 };
