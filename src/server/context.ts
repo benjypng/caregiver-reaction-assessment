522ce8d470c5da2c6b1e82d35410feb35f9d0ffe
hkgnp <ben@pngs.cc>
2024-06-03 07:43:27 +0800

    chore: push

--- src/server/context.ts
+++ src/server/context.ts
@@ -3,11 +3,21 @@ import type { CreateNextContextOptions } from "@trpc/server/adapters/next";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import prisma from "prisma/client";
+import { Session } from "next-auth";
 
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface CreateContextOptions {
+  session: Session | null;
+}
 /**
  * Creates context for an incoming request
  * @link https://trpc.io/docs/context
  */
+
+export async function createContextInner(_opts: CreateContextOptions) {
+  return {};
+}
+
 export async function createContext(opts: CreateNextContextOptions) {
   const session = await getServerSession(opts.req, opts.res, authOptions);
   return {
