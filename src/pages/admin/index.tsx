b3c0c2641743bdfdbca49c6a28dd5091102648cb
hkgnp <ben@pngs.cc>
2024-06-02 17:07:11 +0800

    feat: redirect admin pg if no session

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,58 +1,58 @@
 import { useSession } from "next-auth/react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import FormsTable from "@/components/FormsTable";
 import UserList from "@/components/UserList";
-import { useRouter } from "next/router";
 import { Box, Flex, Spacer, Text } from "@chakra-ui/react";
 import { Button } from "@opengovsg/design-system-react";
+import { useRouter } from "next/navigation";
 
 const AdminDashboard = () => {
   const router = useRouter();
   const [manageUsers, setManageUsers] = useState(false);
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
 
-  useEffect(() => {
-    if (!session) {
-      router.push("/");
-    }
-  }, [session, router]);
+  if (status === "unauthenticated") router.push("/");
 
   return (
-    <Box>
-      <Flex>
-        <Text textStyle="h4" mb="5">
-          Admin Dashboard
-        </Text>
-        <Spacer />
-        {session && session.user.is_admin && (
-          <>
-            {!manageUsers && (
-              <Button
-                variant="ghost"
-                mb="5"
-                mr="3"
-                onClick={() => setManageUsers(true)}
-              >
-                Manage Users
-              </Button>
+    <>
+      {status === "authenticated" && (
+        <Box>
+          <Flex>
+            <Text textStyle="h4" mb="5">
+              Admin Dashboard
+            </Text>
+            <Spacer />
+            {session?.user.is_admin && (
+              <>
+                {!manageUsers && (
+                  <Button
+                    variant="ghost"
+                    mb="5"
+                    mr="3"
+                    onClick={() => setManageUsers(true)}
+                  >
+                    Manage Users
+                  </Button>
+                )}
+                {manageUsers && (
+                  <Button
+                    variant="ghost"
+                    mb="5"
+                    mr="3"
+                    onClick={() => setManageUsers(false)}
+                  >
+                    Manage Forms
+                  </Button>
+                )}
+              </>
             )}
-            {manageUsers && (
-              <Button
-                variant="ghost"
-                mb="5"
-                mr="3"
-                onClick={() => setManageUsers(false)}
-              >
-                Manage Forms
-              </Button>
-            )}
-          </>
-        )}
-      </Flex>
-      <Spacer />
-      {!manageUsers && <FormsTable />}
-      {manageUsers && <UserList session={session} />}
-    </Box>
+          </Flex>
+          <Spacer />
+          {!manageUsers && <FormsTable />}
+          {manageUsers && <UserList session={session} />}
+        </Box>
+      )}
+    </>
   );
 };
 
