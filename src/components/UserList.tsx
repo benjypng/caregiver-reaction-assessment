cbdc58e12e48cf9f688ab33a83d16a325fbbc106
ben <ben@pngs.cc>
2024-06-04 10:53:24 +0800

    fix: user list not loading

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -56,6 +56,7 @@ const UserList = ({ session }: SessionProps) => {
 
   useEffect(() => {
     putUsers();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
